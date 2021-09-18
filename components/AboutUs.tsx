b06605ac1a1e92168b2a9fa573efbb21c72f14f8
Rayner <raynerljm@gmail.com>
2021-09-18 19:20:30 +0800

    update the our challenge page

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -4,66 +4,61 @@ import Challenge from "../components/Challenge";
 
 const AboutUs: FC = ({ children }) => {
   return (
-    <div className="mx-8" id="AboutUs">
+    <div className="mx-8" id="aboutus">
       <div>
-        <Challenge></Challenge>    
+        <Challenge></Challenge>
       </div>
 
       <div>
-
-      <div className="grid grid-cols-2 h-4/5">
-        <div className="flex justify-center items-center h-full">
-          <Image
-            src="/trash.jpeg"
-            alt="Fish Icon"
-            layout="fixed"
-            width={400}
-            height={400}
-          />
-        </div>
-        <div className="flex flex-col justify-center h-full">
-          <h1 className="text-white text-right text-5xl font-semibold">
-            <span className="font-semibold">A Real</span>
-            <br />
-            <span className="font-semibold">Problem</span>
-          </h1>
-
-          <div className ="pl-10">
-          <p
-            className="text-right text-white text-lg font-light mt-6"
-          >
-            microplastic crystals with sizes of a grain of rice down to
-            microscopic are tiny and has been injested by wide range of
-            creatures in the marine wildlife community
-          </p>
-          <p className="text-right text-white text-lg font-light mt-6">According to the UN Environment Programme, 13 million tonnes of plastic leak into our oceans every year, causing an estimated $13 billion of economic damage to global marine ecosystems.
-          </p>
+        <div className="grid grid-cols-2 h-4/5">
+          <div className="flex justify-center items-center h-full">
+            <Image
+              src="/trash.jpeg"
+              alt="Fish Icon"
+              layout="fixed"
+              width={400}
+              height={400}
+            />
           </div>
+          <div className="flex flex-col justify-center h-full">
+            <h1 className="text-white text-right text-5xl font-semibold">
+              <span className="font-semibold">A Real</span>
+              <br />
+              <span className="font-semibold">Problem</span>
+            </h1>
+
+            <div className="pl-10">
+              <p className="text-right text-white text-lg font-light mt-6">
+                microplastic crystals with sizes of a grain of rice down to
+                microscopic are tiny and has been injested by wide range of
+                creatures in the marine wildlife community
+              </p>
+              <p className="text-right text-white text-lg font-light mt-6">
+                According to the UN Environment Programme, 13 million tonnes of
+                plastic leak into our oceans every year, causing an estimated
+                $13 billion of economic damage to global marine ecosystems.
+              </p>
+            </div>
           </div>
-          <br/>
+          <br />
         </div>
       </div>
 
       <div>
-        <br/>
-        <br/>
-        <br/>
+        <br />
+        <br />
+        <br />
       </div>
       <div>
-        <br/>
-      <h1 className="text-white text-center text-5xl font-semibold">
-            <span className="font-semibold">Threat to the sea community</span>
-          </h1>
-          <p
-            className="text-right text-white text-lg font-light mt-6"
-          >
-            microplastic crystals with sizes of a grain of rice down to
-            microscopic are tiny and has been injested by wide range of
-            creatures in the marine wildlife community
-          </p>
-
-
-
+        <br />
+        <h1 className="text-white text-center text-5xl font-semibold">
+          <span className="font-semibold">Threat to the sea community</span>
+        </h1>
+        <p className="text-right text-white text-lg font-light mt-6">
+          microplastic crystals with sizes of a grain of rice down to
+          microscopic are tiny and has been injested by wide range of creatures
+          in the marine wildlife community
+        </p>
       </div>
     </div>
   );
--- components/Challenge.tsx
+++ components/Challenge.tsx
@@ -3,28 +3,12 @@ import { FC } from "react";
 const Challenge: FC = ({ children }) => {
   return (
     <>
-      <div className="w-screen flex justify-center">
-        <div className="max-w-7xl pr-16 pl-32 h-full w-full relative">
-          {children}
-          <br />
-          <br />
-          <h1 className="text-white text-centre text-7xl font-semibold">
-            <span className="font-semibold">Our Challenge</span>
-            <div className="pl-8">
-              <p className="text-left text-white text-lg font-light mt-5">
-                Waste Management: Hidden Microplastic Crystals
-              </p>
-            </div>
-          </h1>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-        </div>
-      </div>
+      <h1 className="mb-4 text-6xl font-semibold text-center text-white">
+        Our Challenge
+      </h1>
+      <p className="text-center text-white text-lg font-light mt-8 mb-16">
+        Waste Management: Hidden Microplastic Crystals
+      </p>
     </>
   );
 };
